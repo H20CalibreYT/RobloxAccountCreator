7cc65b35b636e8b16ba5afa7af13bd07dadd14c5
SystemCalix <41382356+H20CalibreYT@users.noreply.github.com>
2023-11-14 10:10:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,135 +1,180 @@
 import secrets
 import string
 import time
-import sys
 import os
+import sys
+from datetime import date
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-from datetime import date
 import requests
 
-git = "https://raw.githubusercontent.com/H20CalibreYT/RobloxAccountCreator/main/"
-FNurl = git+"firstnames.txt"
-LNurl = git+"lastnames.txt"
+def status(text):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("\033[1;34m" + text + "\033[0m")
 
-FNResponse = requests.get(FNurl)
-LNResponse = requests.get(LNurl)
+#Config
+Accounts = 999999 #how many accounts
 
-if FNResponse.status_code == 200 and LNResponse.status_code == 200:
-    FirstNames = list(set(FNResponse.text.splitlines()))
-    LastNames = list(set(LNResponse.text.splitlines()))
+# URLs
+first_names_url = "https://raw.githubusercontent.com/H20CalibreYT/RobloxAccountCreator/main/firstnames.txt"
+last_names_url = "https://raw.githubusercontent.com/H20CalibreYT/RobloxAccountCreator/main/lastnames.txt"
+roblox_url = "https://www.roblox.com/"
+
+status("Getting first names...")
+first_names_response = requests.get(first_names_url)
+status("Getting last names...")
+last_names_response = requests.get(last_names_url)
+
+# Verificar si la carga de nombres fue exitosa
+if first_names_response.status_code == 200 and last_names_response.status_code == 200:
+    first_names = list(set(first_names_response.text.splitlines()))
+    last_names = list(set(last_names_response.text.splitlines()))
 else:
-    print("Name loading failed, re-execute or something idk")
+    status("Name loading failed. Re-Execute the script.")
     sys.exit()
 
-FilesPath = os.path.dirname(os.path.abspath(sys.argv[0]))
-TextFilesFolder = os.path.join(FilesPath,"Accounts")
-TextFile = os.path.join(TextFilesFolder,"Accounts"+str(date.today())+".txt")
-TextFile2 = os.path.join(TextFilesFolder,"AltManagerLogin"+str(date.today())+".txt")
+# Rutas de archivos
+files_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+text_files_folder = os.path.join(files_path, "Accounts")
+text_file = os.path.join(text_files_folder, f"Accounts_{date.today()}.txt")
+text_file2 = os.path.join(text_files_folder, f"AltManagerLogin_{date.today()}.txt")
 
-if not os.path.exists(TextFilesFolder):
-    os.makedirs(TextFilesFolder)
+# Crear carpeta si no existe
+if not os.path.exists(text_files_folder):
+    os.makedirs(text_files_folder)
 
-Days = [str(i+1) for i in range(10,28)]
-Months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
-Years = [str(i+1) for i in range(1980, 2004)]
+# Listas de días, meses y años
+days = [str(i + 1) for i in range(10, 28)]
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+years = [str(i + 1) for i in range(1980, 2004)]
 
-def GenPassword(lenght):
+# Password generator
+def gen_password(length):
+    status("Generating a password...")
     chars = string.ascii_letters + string.digits + "Ññ¿?¡!#$%&/()=\/¬|°_-[]*~+"
-    password = ''.join(secrets.choice(chars) for i in range(lenght))
+    password = ''.join(secrets.choice(chars) for _ in range(length))
     return password
 
-def GenUser():
-    first = secrets.choice(FirstNames)
-    last = secrets.choice(LastNames)
-    Full = first+last+"_"+str(secrets.choice([i for i in range(1,999)])).zfill(3)
-    return Full
+#Username generator
+def gen_user(first_names, last_names):
+    status("Generating a username...")
+    first = secrets.choice(first_names)
+    last = secrets.choice(last_names)
+    full = f"{first}{last}_{secrets.choice([i for i in range(1, 999)]):03}"
+    return full
 
-def CreateAcc(url):
+def create_account(url, first_names, last_names):
     try:
-        CookieFound = False
-        UsernameFound = False
-        ElapsedTime = 0
+        status("Starting to create an account...")
+        cookie_found = False
+        username_found = False
+        elapsed_time = 0
 
+        status("Initializing webdriver...")
         driver = webdriver.Edge()
         driver.set_window_size(1200, 800)
-        driver.set_window_position(0,0)
+        driver.set_window_position(0, 0)
         driver.get(url)
         time.sleep(2)
+
+        # HTML items
+        status("searching for items on the website")
         username_input = driver.find_element("id", "signup-username")
         username_error = driver.find_element("id", "signup-usernameInputValidation")
         password_input = driver.find_element("id", "signup-password")
         day_dropdown = driver.find_element("id", "DayDropdown")
         month_dropdown = driver.find_element("id", "MonthDropdown")
         year_dropdown = driver.find_element("id", "YearDropdown")
-        sex_button = driver.find_element("id", "MaleButton")
-        register_button = driver.find_element("id","signup-button")
+        male_button = driver.find_element("id", "MaleButton")
+        female_button = driver.find_element("id", "FemaleButton")
+        register_button = driver.find_element("id", "signup-button")
+
+        status("Selecting day...")
         Selection = Select(day_dropdown)
-        Selection.select_by_value(secrets.choice(Days))
+        Selection.select_by_value(secrets.choice(days))
         time.sleep(0.3)
 
+        status("Selecting month...")
         Selection = Select(month_dropdown)
-        Selection.select_by_value(secrets.choice(Months))
+        Selection.select_by_value(secrets.choice(months))
         time.sleep(0.3)
 
+        status("Selecting year...")
         Selection = Select(year_dropdown)
-        Selection.select_by_value(secrets.choice(Years))
+        Selection.select_by_value(secrets.choice(years))
         time.sleep(0.3)
 
-        while not UsernameFound:
-            Username = GenUser()
+        while not username_found:
+            status("Selecting username...")
+            username = gen_user(first_names, last_names)
             username_input.clear()
-            username_input.send_keys(Username)
+            username_input.send_keys(username)
             time.sleep(1)
             if username_error.text.strip() == "":
-                UsernameFound = True
+                username_found = True
         
-        Password = GenPassword(25)
-        password_input.send_keys(Password)
+        status("Selecting password...")
+        password = gen_password(25)
+        password_input.send_keys(password)
         time.sleep(0.3)
 
-        sex_button.click()
+        status("Selecting gender...")
+        gender = secrets.choice([1,2])
+        if gender == 1:
+            male_button.click()
+        else:
+            female_button.click()
         time.sleep(0.5)
 
+        status("Registering account...")
         register_button.click()
         time.sleep(3)
 
+        # Wait until the account creation limit is reset
         try:
-            driver.find_element("id","GeneralErrorText")
+            driver.find_element("id", "GeneralErrorText")
             driver.quit()
-            print("IN COOLDOWN")
             for i in range(360):
-                print(f"in cooldown {i+1}/{360}")
+                status(f"Limit reached, waiting... {i+1}/{360}")
                 time.sleep(1)
         except:
-            print()
-        
-        while (not CookieFound) and (ElapsedTime < 180):
+            pass
+
+        # Wait until the cookie is found or the maximum time has passed
+        while not cookie_found and elapsed_time < 180:
+            status("Waiting for the cookie...")
             time.sleep(3)
-            ElapsedTime += 3
+            elapsed_time += 3
             for cookie in driver.get_cookies():
                 if cookie.get('name') == '.ROBLOSECURITY':
-                    CookieFound = True
+                    cookie_found = True
                     break
-        if CookieFound:
-            return [cookie.get('value'),Username,Password]
-        
-    except Exception as e:
-        print("Ignore me",e)
-    finally:
-        print("Ignore me too")
+        if cookie_found:
+            status("Cookie found...")
+            return [cookie.get('value'), username, password]
 
+    except Exception as e:
+        pass
+    finally:
+        pass
+
+# Save account information to text file
 def save_account_info(account_info):
-    with open(TextFile, 'a') as file:
+    status("Saving account info...")
+    with open(text_file, 'a') as file:
         file.write(f"Username: {account_info[1]}\nPassword: {account_info[2]}\nCookie: {account_info[0]}\n\n\n")
 
-def save_altmanger_login(account_info):
-    with open(TextFile2, 'a') as file:
+# Save login information for AltManager
+def save_altmanager_login(account_info):
+    with open(text_file2, 'a') as file:
+        status("Saving account login (for alt manager)...")
         file.write(f"{account_info[1]}:{account_info[2]}\n")
 
-for i in range(999999):
-    Account = CreateAcc("https://www.roblox.com/")
-    if Account != None:
-        save_account_info(Account)
-        save_altmanger_login(Account)
+# Create accounts
+for _ in range(Accounts):
+    account = create_account(roblox_url, first_names, last_names)
+    if account is not None:
+        save_account_info(account)
+        save_altmanager_login(account)
+        status("Successfully created!")
         time.sleep(3)
